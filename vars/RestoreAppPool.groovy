9545fc31a42059b72af3ab9a526b857c3a11b335
hareeshkmurali <44630355+hareeshkmurali@users.noreply.github.com>
2023-05-24 17:54:30 +0530

    Update RestoreAppPool.groovy

--- vars/RestoreAppPool.groovy
+++ vars/RestoreAppPool.groovy
@@ -1,21 +1,29 @@
-def runPowerShellScript(String userName, String password, String computerName, String siteName) {
-    powershell """
-    param (
-        [string]$UserName,
-        [string]$Password,
-        [string]$ComputerName,
-        [string]$SiteName
-    )
+def call(Map scriptParams) {
+    def userName = scriptParams.UserName
+    def password = scriptParams.Password
+    def computerName = scriptParams.ComputerName
+    def siteName = scriptParams.SiteName
+
+    // PowerShell script content
+    def powerShellScript = """
+        Param(
+            [string]$$UserName,
+            [string]$$Password,
+            [string]$$ComputerName,
+            [string]$$SiteName
+        )
     
-    $secStringPassword = ConvertTo-SecureString $Password -AsPlainText -Force
-    $credObject = New-Object System.Management.Automation.PSCredential ($UserName, $secStringPassword)
+    \$secStringPassword = ConvertTo-SecureString ${Password} -AsPlainText -Force
+    \$credObject = New-Object System.Management.Automation.PSCredential (${UserName}, ${secStringPassword})
     
-    Invoke-Command -ComputerName $ComputerName -Credential $credObject -ScriptBlock {
+    Invoke-Command -ComputerName ${ComputerName} -Credential $credObject -ScriptBlock {
         param($SiteName)
         
         Import-Module WebAdministration
-        $appPool = Get-Item "IIS:\\Sites\\$SiteName" | Select-Object -ExpandProperty applicationPool
+        \$appPool = Get-Item "IIS:\\Sites\\$SiteName" | Select-Object -ExpandProperty applicationPool
         Start-WebAppPool $appPool
     } -ArgumentList $SiteName
     """
+
+    def result = powershell(returnStdout: true, script: powerShellScript)
 }
