26485c5eb301a1a3a4fe7655c560ec6d631e1c81
hareeshkmurali <44630355+hareeshkmurali@users.noreply.github.com>
2023-05-24 16:43:20 +0530

    Update DeploytoIIS.groovy

--- vars/DeploytoIIS.groovy
+++ vars/DeploytoIIS.groovy
@@ -1,10 +1,43 @@
-def call(String userName, String password, String computerName, String siteName, String webUrl, String workspace, String buildPath) {
-  def msdeployCommand = "msdeploy -verb:sync -source:recycleApp -dest:recycleApp=\"$siteName\",computerName=\"$webUrl\",recycleMode=\"StopAppPool\",authType=\"basic\",userName=\"$userName\",password=\"$password\" -allowUntrusted=true"
-  bat msdeployCommand
-  
-  def msdeployCommand2 = "msdeploy -verb=sync -source:contentPath=\"$workspace\\$buildPath\" -dest:contentPath=\"$siteName\",computerName=\"$webUrl\",userName=\"$userName\",password=\"$password\",authType=\"basic\" -allowUntrusted=true -enableRule:DoNotDeleteRule -skip:objectName=filePath,absolutePath=\".*\\.csv$\" -skip:objectName=filePath,absolutePath=\".*\\.pdf$\" -skip:objectName=filePath,absolutePath=\".*\\.xls$\" -skip:objectName=filePath,absolutePath=\".*\\.xlsx$\" -skip:objectName=filePath,absolutePath=\"$siteName\\\\Web\\.config$\",skipAction=Update"
-  bat msdeployCommand2
-  
-  def msdeployCommand3 = "msdeploy -verb:sync -source:recycleApp -dest:recycleApp=\"$siteName\",computerName=\"$webUrl\",recycleMode=\"StartAppPool\",authType=\"basic\",userName=\"$userName\",password=\"$password\" -allowUntrusted=true"
-  bat msdeployCommand3
+def runPowerShellScript(String userName, String password, String computerName, String siteName, String webUrl, String workspace, String buildPath) {
+    powershell """
+    Param (
+        [string]\$UserName,
+        [string]\$Password,
+        [string]\$ComputerName,
+        [string]\$SiteName,
+        [string]\$WebUrl,
+        [string]\$Workspace,
+        [string]\$BuildPath
+    )
+
+    \$msdeployArguments = @(
+        "-verb:sync",
+        "-source:recycleApp",
+        "-dest:recycleApp=`"\$SiteName`",computerName=`"\$WebUrl`",recycleMode=`"StopAppPool`",authType=`"basic`",userName=`"\$UserName`",password=`"\$Password`"",
+        "-allowUntrusted=true"
+    )
+    Start-Process -FilePath 'msdeploy' -ArgumentList \$msdeployArguments -Wait
+
+    \$msdeployArguments = @(
+        "-verb=sync",
+        "-source:contentPath=`"\$Workspace\\\$BuildPath`"",
+        "-dest:contentPath=`"\$SiteName`",computerName=`"\$WebUrl`",userName=`"\$UserName`",password=`"\$Password`",authType=`"basic`"",
+        "-allowUntrusted=true",
+        "-enableRule:DoNotDeleteRule",
+        "-skip:objectName=filePath,absolutePath=`".*\\.csv\$`"",
+        "-skip:objectName=filePath,absolutePath=`".*\\.pdf\$`"",
+        "-skip:objectName=filePath,absolutePath=`".*\\.xls\$`"",
+        "-skip:objectName=filePath,absolutePath=`".*\\.xlsx\$`"",
+        "-skip:objectName=filePath,absolutePath=`"\$SiteName\\Web\\.config\$`",skipAction=Update"
+    )
+    Start-Process -FilePath 'msdeploy' -ArgumentList \$msdeployArguments -Wait
+
+    \$msdeployArguments = @(
+        "-verb:sync",
+        "-source:recycleApp",
+        "-dest:recycleApp=`"\$SiteName`",computerName=`"\$WebUrl`",recycleMode=`"StartAppPool`",authType=`"basic`",userName=`"\$UserName`",password=`"\$Password`"",
+        "-allowUntrusted=true"
+    )
+    Start-Process -FilePath 'msdeploy' -ArgumentList \$msdeployArguments -Wait
+    """
 }
