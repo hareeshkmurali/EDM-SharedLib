1e2f26b0e5fa5632833f705283ffa16c2c3b6607
hareeshkmurali <44630355+hareeshkmurali@users.noreply.github.com>
2023-05-24 14:51:24 +0530

    Add files via upload

---
+++ vars/Restore-AppPool.groovy
@@ -0,0 +1,21 @@
+def runPowerShellScript(String userName, String password, String computerName, String siteName) {
+    powershell """
+    param (
+        [string]$UserName,
+        [string]$Password,
+        [string]$ComputerName,
+        [string]$SiteName
+    )
+    
+    $secStringPassword = ConvertTo-SecureString $Password -AsPlainText -Force
+    $credObject = New-Object System.Management.Automation.PSCredential ($UserName, $secStringPassword)
+    
+    Invoke-Command -ComputerName $ComputerName -Credential $credObject -ScriptBlock {
+        param($SiteName)
+        
+        Import-Module WebAdministration
+        $appPool = Get-Item "IIS:\\Sites\\$SiteName" | Select-Object -ExpandProperty applicationPool
+        Start-WebAppPool $appPool
+    } -ArgumentList $SiteName
+    """
+}
