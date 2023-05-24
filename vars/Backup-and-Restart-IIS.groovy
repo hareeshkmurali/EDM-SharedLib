7dd671369829e57035713851a73b604df4e95d09
hareeshkmurali <44630355+hareeshkmurali@users.noreply.github.com>
2023-05-24 16:22:47 +0530

    Update Backup-and-Restart-IIS.groovy

--- vars/Backup-and-Restart-IIS.groovy
+++ vars/Backup-and-Restart-IIS.groovy
@@ -1,50 +1,35 @@
-// myScript.groovy
+def runPowerShellScript(String buildNumber, String userName, String password, String computerName, String siteName, String sourcePath, String directoryPath) {
+    powershell """
+    param (
+        [string]$BuildNumber,
+        [string]$UserName,
+        [string]$Password,
+        [string]$ComputerName,
+        [string]$SiteName,
+        [string]$SourcePath,
+        [string]$DirectoryPath
+    )
 
-def call(Map scriptParams) {
-    def BuildNumber = scriptParams.BuildNumber
-    def UserName = scriptParams.UserName
-    def Password = scriptParams.Password
-    def ComputerName = scriptParams.ComputerName
-    def SiteName = scriptParams.SiteName
-    def SourcePath = scriptParams.SourcePath
-    def DirectoryPath = scriptParams.DirectoryPath
+    $ErrorActionPreference = "Stop"
+    $secStringPassword = ConvertTo-SecureString $Password -AsPlainText -Force
+    $credObject = New-Object System.Management.Automation.PSCredential ($UserName, $secStringPassword)
 
-    // PowerShell script content
-    def powerShellScript = """
-        Param(
-            [string]$$BuildNumber,
-            [string]$$UserName,
-            [string]$$Password,
-            [string]$$ComputerName,
-            [string]$$SiteName,
-            [string]$$SourcePath,
-            [string]$$DirectoryPath
-        )
+    Invoke-Command -ComputerName $ComputerName -Credential $credObject -ScriptBlock {
+        param($BuildNumber, $SiteName, $SourcePath, $DirectoryPath)
 
-        \$ErrorActionPreference = "Stop"
-        \$secStringPassword = ConvertTo-SecureString ${Password} -AsPlainText -Force
-        \$credObject = New-Object System.Management.Automation.PSCredential (${UserName}, ${secStringPassword})
+        \$Files = Get-ChildItem -Path \$DirectoryPath -File
+        \$FilesSorted = \$Files | Sort-Object -Property CreationTime -Descending
+        \$FilesToKeep = \$FilesSorted | Select-Object -First 5
+        \$FilesSorted | Where-Object { \$_.FullName -notin \$FilesToKeep.FullName } | Remove-Item -Force
 
-        Invoke-Command -ComputerName ${ComputerName} -Credential ${credObject} -ScriptBlock {
-            param(\$BuildNumber, \$SiteName, \$SourcePath, \$DirectoryPath)
+        Import-Module WebAdministration
+        \$appPool = Get-Item "IIS:\\Sites\\$SiteName" | Select-Object -ExpandProperty applicationPool
+        Restart-WebAppPool \$appPool
 
-            \$Files = Get-ChildItem -Path ${DirectoryPath} -File
-            \$FilesSorted = ${Files} | Sort-Object -Property CreationTime -Descending
-            \$FilesToKeep = ${FilesSorted} | Select-Object -First 5
-            \$FilesSorted | Where-Object { \$.FullName -notin \$FilesToKeep.FullName } | Remove-Item -Force
-
-            Import-Module WebAdministration
-            \$appPool = Get-Item "IIS:\\Sites\\\$SiteName" | Select-Object -ExpandProperty applicationPool
-            Restart-WebAppPool \$appPool
-
-            \$source="\$SourcePath"
-            \$dest="\$DirectoryPath\\Archive-\$BuildNumber.zip"
-            Add-Type -assembly "system.io.compression.filesystem"
-            [io.compression.zipfile]::CreateFromDirectory(\$source, \$dest)
-        } -ArgumentList \$BuildNumber, \$SiteName, \$SourcePath, \$DirectoryPath
+        \$source = "$SourcePath"
+        \$dest = "$DirectoryPath\\Archive-\$BuildNumber.zip"
+        Add-Type -assembly "system.io.compression.filesystem"
+        [io.compression.zipfile]::CreateFromDirectory(\$source, \$dest)
+    } -ArgumentList $buildNumber, $siteName, $sourcePath, $directoryPath
     """
-
-    // Execute the PowerShell script
-    def result = powershell(returnStdout: true, script: powerShellScript)
-
 }
