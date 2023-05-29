0c0a9dda34bb6c46087a449d96ebe23a1d67c159
hareeshkmurali <44630355+hareeshkmurali@users.noreply.github.com>
2023-05-29 14:00:06 +0530

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,3 +1,5 @@
 def call(def psw) {
-    println psw
+    
+    writeFile file: 'test1.ps1', text: "${libraryResource 'CommonScripts/test.ps1'}"
+    bat "powershell -ExecutionPolicy ByPass -File test1.ps1 $psw"
 }
