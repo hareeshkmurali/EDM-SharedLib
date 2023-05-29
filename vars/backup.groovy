4289936ceb1324faba518d3c2c98d516b2dd79a7
hareeshkmurali <44630355+hareeshkmurali@users.noreply.github.com>
2023-05-29 12:17:07 +0530

    Update backup.groovy

--- vars/backup.groovy
+++ vars/backup.groovy
@@ -1,4 +1,9 @@
 def call(String BuildNumber, String UserName, def Password, String ComputerName, String SiteName, String SourcePath, String DirectoryPath) {
+    def percent = '%'
+    def greaterThan = '>'
+
+    println percent // Output: %
+    println greaterThan // Output: >
     writeFile file: 'backup.ps1', text: "${libraryResource 'CommonScripts/Backup-and-Restart-IIS.ps1'}"
     bat "powershell -ExecutionPolicy ByPass -File backup.ps1 $BuildNumber $UserName $Password $ComputerName $SiteName $SourcePath $DirectoryPath"
 }
