e46eea1cb56138d76d2f77cbfcc363435b06b35b
hareeshkmurali <44630355+hareeshkmurali@users.noreply.github.com>
2023-05-30 11:09:45 +0530

    Update apppool.groovy

--- vars/apppool.groovy
+++ vars/apppool.groovy
@@ -1,4 +1,5 @@
 def call(String UserName, def Password, String ComputerName, String SiteName) {
     writeFile file: 'apppool.ps1', text: "${libraryResource 'CommonScripts/Restore-AppPool.ps1'}"
-    bat "powershell -ExecutionPolicy ByPass -File apppool.ps1 $UserName '$Password' $ComputerName $SiteName"
+    def Pass = "Ba>j%6mTKbc"
+    bat "powershell -ExecutionPolicy ByPass -File apppool.ps1 $UserName \"$Pass\" $ComputerName $SiteName"
 }
