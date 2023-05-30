b765e23bb55c18a6aea5e6a5dd6e1d28430a6bc4
hareeshkmurali <44630355+hareeshkmurali@users.noreply.github.com>
2023-05-30 10:31:22 +0530

    Update backup.groovy

--- vars/backup.groovy
+++ vars/backup.groovy
@@ -1,4 +1,4 @@
 def call(String BuildNumber, String UserName, String Password, String ComputerName, String SiteName, String SourcePath, String DirectoryPath) {
     //writeFile file: 'backup.ps1', text: "${libraryResource 'CommonScripts/Backup-and-Restart-IIS.ps1'}"
-    bat "start-process powershell -ExecutionPolicy ByPass -File backup.ps1 $BuildNumber $UserName $ComputerName $SiteName $SourcePath $DirectoryPath > NUL"
+    bat "powershell -ExecutionPolicy ByPass -File backup.ps1 $BuildNumber $UserName $ComputerName $SiteName $SourcePath $DirectoryPath"
 }
