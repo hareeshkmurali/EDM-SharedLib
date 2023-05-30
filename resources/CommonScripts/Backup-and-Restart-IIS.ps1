9d30ec280a05cf12c54c84110b9b38b1e36558be
hareeshkmurali <44630355+hareeshkmurali@users.noreply.github.com>
2023-05-30 14:49:41 +0530

    Update Backup-and-Restart-IIS.ps1

--- resources/CommonScripts/Backup-and-Restart-IIS.ps1
+++ resources/CommonScripts/Backup-and-Restart-IIS.ps1
@@ -1,7 +1,5 @@
 Param(
     [Parameter(Mandatory=$true)]
-    [string]$UserName,
-    [Parameter(Mandatory=$true)]
     [string]$ComputerName,
     [Parameter(Mandatory=$true)]
     [string]$SiteName,
@@ -15,7 +13,7 @@ $ErrorActionPreference = "Stop"
 
 $Password = $env:IISPASWD
 $secStringPassword = ConvertTo-SecureString $Password -AsPlainText -Force
-$credObject = New-Object System.Management.Automation.PSCredential ($UserName, $secStringPassword)
+$credObject = New-Object System.Management.Automation.PSCredential (${env.UserName}, $secStringPassword)
 
 Invoke-Command -ComputerName $ComputerName -Credential $credObject -ScriptBlock {
     param(${env.BUILD_NUMBER}, $SiteName, $SourcePath, $DirectoryPath)
