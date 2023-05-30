99f5153c85f700af41413a2e406ed17c0a148598
hareeshkmurali <44630355+hareeshkmurali@users.noreply.github.com>
2023-05-30 15:47:55 +0530

    Update Backup-and-Restart-IIS.ps1

--- resources/CommonScripts/Backup-and-Restart-IIS.ps1
+++ resources/CommonScripts/Backup-and-Restart-IIS.ps1
@@ -1,37 +1,30 @@
-Param(
-    [Parameter(Mandatory=$true)]
-    [string]$ComputerName,
-    [Parameter(Mandatory=$true)]
-    [string]$SiteName,
-    [Parameter(Mandatory=$true)]
-    [string]$SourcePath,
-    [Parameter(Mandatory=$true)]
-    [string]$DirectoryPath
-)
-
 $ErrorActionPreference = "Stop"
 $BuildNumber = $env:BUILD_NUMBER
 $Password = $env:IISPASWD
 $uname = $env:UserName
-$secStringPassword = ConvertTo-SecureString ${env:IISPASWD} -AsPlainText -Force
-$credObject = New-Object System.Management.Automation.PSCredential (${env:UserName}, $secStringPassword)
+$CompName = $env:ComputerName
+$WSName = $env:SiteName
+$SrcName = $env:SourcePath
+$DirPath = $env:DirectoryPath
+$secStringPassword = ConvertTo-SecureString $Password -AsPlainText -Force
+$credObject = New-Object System.Management.Automation.PSCredential ($uname, $secStringPassword)
 
-Invoke-Command -ComputerName $ComputerName -Credential $credObject -ScriptBlock {
-    param($BuildNumber, $SiteName, $SourcePath, $DirectoryPath)
+Invoke-Command -ComputerName $CompName -Credential $credObject -ScriptBlock {
+    param($BuildNumber, $WSName, $SrcName, $DirPath)
 
-    $Files = Get-ChildItem -Path $DirectoryPath -File
+    $Files = Get-ChildItem -Path $DirPath -File
     $FilesSorted = $Files | Sort-Object -Property CreationTime -Descending
     $FilesToKeep = $FilesSorted | Select-Object -First 5
     $FilesSorted | Where-Object { $_.FullName -notin $FilesToKeep.FullName } | Remove-Item -Force
     
     Import-Module WebAdministration
-    $appPool = Get-Item "IIS:\\Sites\\$SiteName" | Select-Object -ExpandProperty applicationPool
+    $appPool = Get-Item "IIS:\\Sites\\$WSName" | Select-Object -ExpandProperty applicationPool
     Restart-WebAppPool $appPool
 
-    $source="$SourcePath"
-    $dest="$DirectoryPath\\Archive-$env:BUILD_NUMBER.zip"
+    $source="$SrcName"
+    $dest="$DirPath\\Archive-$BuildNumber.zip"
     Add-Type -assembly "system.io.compression.filesystem"
     [io.compression.zipfile]::CreateFromDirectory($source, $dest)
-} -ArgumentList $BuildNumber, $SiteName, $SourcePath, $DirectoryPath
+} -ArgumentList $BuildNumber, $WSName, $SrcName, $DirPath
 
 
