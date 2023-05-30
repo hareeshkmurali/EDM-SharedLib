65658debdb727d2d0f3923affc097d2236fafc99
hareeshkmurali <44630355+hareeshkmurali@users.noreply.github.com>
2023-05-30 11:13:34 +0530

    Update Restore-AppPool.ps1

--- resources/CommonScripts/Restore-AppPool.ps1
+++ resources/CommonScripts/Restore-AppPool.ps1
@@ -2,13 +2,20 @@ Param(
     [Parameter(Mandatory=$true)]
     [string]$UserName,
     [Parameter(Mandatory=$true)]
-    [string]$Password,
+    [string]$Pass,
     [Parameter(Mandatory=$true)]
     [string]$ComputerName,
     [Parameter(Mandatory=$true)]
     [string]$SiteName
 )
 # $Password = $Password.Trim('"')
+Write-Output $Pass
+$Pass1 = "Ba>j%6mTKbc"
+if ($Pass -eq $Pass1) {
+    Write-Host "Strings are equal."
+} else {
+    Write-Host "Strings are not equal."
+}
 $secStringPassword = ConvertTo-SecureString $Password -AsPlainText -Force
 $credObject = New-Object System.Management.Automation.PSCredential ($UserName, $secStringPassword)
 
