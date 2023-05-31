b410045227a3177abfc8436276b97171e7a19a50
hareeshkmurali <44630355+hareeshkmurali@users.noreply.github.com>
2023-05-31 12:23:53 +0530

    Update WebFileCount.ps1

--- resources/CommonScripts/WebFileCount.ps1
+++ resources/CommonScripts/WebFileCount.ps1
@@ -6,7 +6,8 @@ $SrcName = $env:SourcePath
 $secStringPassword = ConvertTo-SecureString $Password -AsPlainText -Force
 $credObject = New-Object System.Management.Automation.PSCredential ($uname, $secStringPassword)
 
-Invoke-Command -ComputerName $CompName -Credential $credObject -ScriptBlock {
+$result = Invoke-Command -ComputerName $CompName -Credential $credObject -ScriptBlock {
   param($SrcName)
   (Get-ChildItem -Path $SrcName -Recurse -File | Measure-Object).Count
  } -ArgumentList $SrcName
+ Write-Host "IIS Directory File Count: $result"
