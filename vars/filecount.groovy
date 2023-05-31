48b385d39d0318d68e67e558e65004e85a31d8a0
hareeshkmurali <44630355+hareeshkmurali@users.noreply.github.com>
2023-05-31 12:05:57 +0530

    Update filecount.groovy

--- vars/filecount.groovy
+++ vars/filecount.groovy
@@ -1,7 +1,4 @@
 def call() {
-    
     writeFile file: 'filecount.ps1', text: "${libraryResource 'CommonScripts/BuildFileCount.ps1'}"
-    def powershellScript = "powershell -ExecutionPolicy ByPass -File filecount.ps1"
-    def scriptOutput = bat(returnStdout: true, script: powershellScript)
-    return scriptOutput
+    bat "powershell -ExecutionPolicy ByPass -File filecount.ps1"
 }
